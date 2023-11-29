b08df7106f2392c0ed9f279653052b471b218547
KundanKumar333 <kundankumarsingh2005@gmail.com>
2023-11-29 10:33:28 +0530

    third setup

--- anurag.js
+++ anurag.js
@@ -4,3 +4,13 @@ food.addEventListener("click",function(){
     // alert("clik")
     window.location.assign("nikhlesh.html")
 })
+
+
+
+
+let groc=document.querySelector("grocery");
+
+groc.addEventListener("click",function(){
+    // alert("clik")
+    window.location.assign("nikhlesh1.html")
+})
